fbb235cdd1d554425759811d0846c9afc4ee7889
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-04 12:38:52 +0530

    Update UsersController.php

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -198,7 +198,7 @@ class UsersController extends BaseController
                 $token->delete();
             }
             $user->save();
-            $token = Auth::user()->createToken('myApp')->accessToken;
+            $token = Auth::user()->createToken('appToken')->accessToken;
 
             $user_profile = UserProfile::where('user_id', $user->id)->first();
             $zodiac = UserDetail::where('user_id', $user->id)->select('zodiac_sign_id')->exists();
