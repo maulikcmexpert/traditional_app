ee7f68773a533f0d01c2d001c1193fbaae908291
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-06 19:03:24 +0530

    Update getData.php

--- app/Helper/getData.php
+++ app/Helper/getData.php
@@ -7,11 +7,12 @@ function getManageRequest($type, $receiver_id)
 {
     $request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->get();
     $userData = [];
+    dd($request);
     foreach ($request as $val) {
         $userInfo['id'] = $val->sender_id;
         $userInfo['name'] = $val->sender_user->full_name;
         $getProfile = UserProfile::where(['user_id' => $val->sender_id, 'is_default' => '1'])->first();
-        dd($getProfile);
+
         $userInfo['profile'] = $val->receiver_user->full_name;
     }
 }
