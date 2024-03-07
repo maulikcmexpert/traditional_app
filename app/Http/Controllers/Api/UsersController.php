33307f2da79d4612d4228f969681905b8a5537cf
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-07 19:24:59 +0530

    Update UsersController.php

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -937,9 +937,10 @@ class UsersController extends BaseController
             DB::rollBack();
 
             return response()->json(['status' => false, 'message' => "db error"]);
-        } catch (\Exception $e) {
-            return response()->json(['status' => false, 'message' => "something went wrong"]);
         }
+        // catch (\Exception $e) {
+        //     return response()->json(['status' => false, 'message' => "something went wrong"]);
+        // }
     }
 
     public function getReligion()
