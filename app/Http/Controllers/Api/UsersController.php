3cc178e1250f45c3d2f3047b7185bf24714de801
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-07 19:30:49 +0530

    Update UsersController.php

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -888,8 +888,7 @@ class UsersController extends BaseController
         try {
             DB::beginTransaction();
             if ($request->type == "add_img") {
-                echo $this->user->id;
-                exit;
+
                 if (!empty($request->profile_image)) {
 
                     $checkImageExist = UserProfile::where('user_id', $this->user->id)->orderBy('id', 'desc')->first();
