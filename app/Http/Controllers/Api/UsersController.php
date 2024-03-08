9d3b8450a0aa97f21e909ed5dc0e4246fe64f967
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-08 20:52:52 +0530

    Update UsersController.php

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -58,7 +58,7 @@ class UsersController extends BaseController
 
         $this->perPage = 5;
         $this->user = Auth::guard('api')->user();
-        dd($this->user);
+        dd($this->user->userdetail);
     }
 
 
