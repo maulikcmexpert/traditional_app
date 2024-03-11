ca135257b163656b5b84577d22704438ec4c8f43
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-11 14:52:39 +0530

    Update getData.php

--- app/Helper/getData.php
+++ app/Helper/getData.php
@@ -25,7 +25,7 @@ function getManageRequest($type, $page, $receiver_id)
         $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->count();
         $total_page  = ceil($total_request / 10);
         $request = ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->paginate(10, ['*'], 'page', $page);
-    } elseif ($type = "cancelled") {
+    } elseif ($type = "cancle") {
         $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->count();
         $total_page  = ceil($total_request / 10);
         $request = ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->paginate(10, ['*'], 'page', $page);
