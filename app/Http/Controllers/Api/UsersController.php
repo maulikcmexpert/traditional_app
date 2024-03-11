d91800b70eb738bff8480e2e034cbb687727ca75
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-11 15:46:32 +0530

    Update UsersController.php

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -549,6 +549,8 @@ class UsersController extends BaseController
 
             if ($user_id) {
 
+                $count = Country::where('country_id', $user_id)->first();
+                dd($count);
                 $count = UserDetail::where('organization_id', $user_id)->get();
                 $data['member_count'] = (count($count) != "") ? count($count) : "";
                 $organization_detail = OrganizationDetail::where('organization_id', $user_id)->get();
