c46d4cbdba497e81cbf3f49b95ac1e81baa71f79
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-12 16:03:25 +0530

    Update getData.php

--- app/Helper/getData.php
+++ app/Helper/getData.php
@@ -20,7 +20,7 @@ function getManageRequest($type, $page, $receiver_id)
 
     $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->count();
     $total_page  = ceil($total_request / 10);
-    $request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->paginate(10, ['*'], 'page', $page);
+    $request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->orderBy('created_at', 'desc')->paginate(10, ['*'], 'page', $page);
     if ($type == 'rejected') {
         $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->orderBy('created_at', 'desc')->onlyTrashed()->count();
         $total_page  = ceil($total_request / 10);
