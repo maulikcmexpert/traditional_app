0720325fa6f876e4be252ac18db4ad62425a80b7
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-12 15:44:56 +0530

    Update UsersController.php

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -1316,8 +1316,9 @@ class UsersController extends BaseController
 
 
             $checkIsApproched =  ApproachRequest::where(['sender_id' => $this->user->id, 'receiver_id' => $request->user_id])->withTrashed()->orderBy('id', 'DESC')->first();
-            dd($checkIsApproched);
+
             if ($checkIsApproched != null) {
+                dd($checkIsApproched->status);
                 if ($checkIsApproched->status == 'pending') {
 
                     return response()->json(["status" => false, 'message' => 'You have already approach request to this person']);
