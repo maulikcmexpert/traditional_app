aeac8412d65292e2b4ce884c8e780ed9daf965e5
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-12 18:48:09 +0530

    Update UserShwstpprQue.php

--- app/Models/UserShwstpprQue.php
+++ app/Models/UserShwstpprQue.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\{
     User,
     UserShwstpperAnswr
@@ -12,7 +13,8 @@ use App\Models\{
 class UserShwstpprQue extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
     public function user()
     {
         return $this->belongsTo(User::class);
