7b9dc14aca6df96f49da0eb1007048d69eded20f
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-15 19:36:04 +0530

    Update OrganizationNameValidation.php

--- app/Rules/OrganizationNameValidation.php
+++ app/Rules/OrganizationNameValidation.php
@@ -15,7 +15,7 @@ class OrganizationNameValidation implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (ctype_digit(trim($value))) {
-            $fail("Organization Name should not be only digits");
+            $fail("Church/Organization Name should not be only digits");
         }
 
         if (preg_match("/^[^a-zA-Z0-9 ]+$/", trim($value))) {
