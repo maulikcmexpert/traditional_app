5e13ece0b0580ff62e532da36233c531ae908ffd
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-15 19:03:28 +0530

    Update CustomEmailValidation.php

--- app/Rules/CustomEmailValidation.php
+++ app/Rules/CustomEmailValidation.php
@@ -10,16 +10,7 @@ class CustomEmailValidation implements Rule
     public function passes($attribute, $value)
     {
         // Check if the email starts with a character
-        $pattern = '/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/';
-        if (!preg_match($pattern, $value)) {
-            return false;
-        }
-
-        // Check if the username part of the email doesn't consist entirely of digits
-        // $valueSplit = explode('@', $value);
-        // if (ctype_digit($valueSplit[0])) {
-        //     return false;
-        // }
+        return preg_match('/^[A-Za-z]/', $value) === 1;
     }
 
     public function message()
