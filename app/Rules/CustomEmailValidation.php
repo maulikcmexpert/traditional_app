42246af33b5379bc5c1cff8d884d38c389ab4c62
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-15 15:01:48 +0530

    Update CustomEmailValidation.php

--- app/Rules/CustomEmailValidation.php
+++ app/Rules/CustomEmailValidation.php
@@ -11,11 +11,13 @@ class CustomEmailValidation implements Rule
     {
         if (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $value)) {
             return false;
-        } elseif (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z]+[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $value)) {
-            return false;
-        } elseif (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}[a-zA-Z0-9]*$/', $value)) {
-            return false;
         }
+
+        // elseif (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z]+[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $value)) {
+        //     return false;
+        // } elseif (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}[a-zA-Z0-9]*$/', $value)) {
+        //     return false;
+        // }
     }
 
     public function message()
