9e5dd7631a2a718a12fca00d907d3d9c66b19d5b
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-19 17:54:41 +0530

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:1000000000000,1',
-            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
+
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
