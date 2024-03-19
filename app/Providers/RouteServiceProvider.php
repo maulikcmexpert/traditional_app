32a46ca42833b92f5ef740e95b04b4d9756761be
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-19 17:27:19 +0530

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,10 +24,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // RateLimiter::for('api', function (Request $request) {
-        //     return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        // });
-        // $this->configureRateLimiting();
+
 
         $this->routes(function () {
             Route::middleware('api')
