72d1abcbe88a0279c3b18ac477b793de223aec51
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-20 16:18:57 +0530

    commit

---
+++ database/migrations/2024_03_19_062439_create_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('ios_version')->nullable();
+            $table->string('android_version')->nullable();
+            $table->string('min_age')->nullable();
+            $table->string('max_age')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
