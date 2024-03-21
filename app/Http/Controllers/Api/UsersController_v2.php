963435ba645586ae0866c54ef28dd14ec4e29c44
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-21 11:43:36 +0530

    Update UsersController_v2.php

--- app/Http/Controllers/Api/UsersController_v2.php
+++ app/Http/Controllers/Api/UsersController_v2.php
@@ -782,6 +782,7 @@ class UsersController_v2 extends BaseController
                     }
                 }
                 $data['is_approach'] = "no_button";
+                dd($this->user->userdetail->gender);
                 if ($this->user->userdetail->gender = 'male' && $user->userdetail->gender == 'female') {
 
                     $approch_check = ApproachRequest::where(['sender_id' => $this->user->id, 'receiver_id' => $user_id])->withTrashed()->orderBy('id', 'DESC')->first();
@@ -865,7 +866,7 @@ class UsersController_v2 extends BaseController
                             $data['is_approach'] = "no_button";
                         }
                     }
-                } else if ($this->user->userdetail->gender = 'male' && $user->userdetail->gender == 'male') {
+                } elseif ($this->user->userdetail->gender = 'male' && $user->userdetail->gender == 'male') {
                     $approch_check = ApproachRequest::where(['sender_id' => $user_id, 'receiver_id' => $this->user->id])->withTrashed()->orderBy('id', 'DESC')->first();
 
 
