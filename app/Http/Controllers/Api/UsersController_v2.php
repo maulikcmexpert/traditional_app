4ad13d84932a59b29964a11b061c7a1d8dd31102
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-21 17:43:57 +0530

    Update UsersController_v2.php

--- app/Http/Controllers/Api/UsersController_v2.php
+++ app/Http/Controllers/Api/UsersController_v2.php
@@ -1040,9 +1040,9 @@ class UsersController_v2 extends BaseController
             if (isset($user_love_lang) && is_array($user_love_lang)) {
                 // if exists then delete prev data //
                 foreach ($user_love_lang as $val) {
-                    dd($val['id']);
-                    $updateUserLoveLang =  UserLoveLang::where('id', $val->id)->first();
-                    $updateUserLoveLang->rate =  $val->rate;
+
+                    $updateUserLoveLang =  UserLoveLang::where('id', $val['id'])->first();
+                    $updateUserLoveLang->rate =  $val['rate'];
                     $updateUserLoveLang->save();
                 }
             }
