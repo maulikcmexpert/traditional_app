fdfcc318df8b8df28335a01e1ca1065522bd369e
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-22 17:45:31 +0530

    Update getData.php

--- app/Helper/getData.php
+++ app/Helper/getData.php
@@ -189,17 +189,17 @@ function getManageRequestByMale($type, $page, $receiver_id)
         ->orderBy('updated_at', 'desc')
         ->paginate(10, ['*'], 'page', $page);
 
-    // if ($type == 'rejected') {
+    if ($type == 'rejected') {
 
-    //     $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->orderBy('updated_at', 'desc')->onlyTrashed()->count();
-    //     $total_page  = ceil($total_request / 10);
-    //     $request = ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->orderBy('updated_at', 'desc')->paginate(10, ['*'], 'page', $page);
-    // } else if ($type == "cancelled") {
+        $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->orderBy('updated_at', 'desc')->onlyTrashed()->count();
+        $total_page  = ceil($total_request / 10);
+        $request = ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->orderBy('updated_at', 'desc')->paginate(10, ['*'], 'page', $page);
+    } else if ($type == "cancelled") {
 
-    //     $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->count();
-    //     $total_page  = ceil($total_request / 10);
-    //     $request = ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->orderBy('updated_at', 'desc')->paginate(10, ['*'], 'page', $page);
-    // }
+        $total_request =  ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->count();
+        $total_page  = ceil($total_request / 10);
+        $request = ApproachRequest::with(['sender_user'])->where(['status' => $type, 'receiver_id' => $receiver_id])->onlyTrashed()->orderBy('updated_at', 'desc')->paginate(10, ['*'], 'page', $page);
+    }
     $userData = [];
     dd($request);
     foreach ($request as $val) {
