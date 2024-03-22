7454cf8661e84873a9f3631182bb57d2125250ce
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-22 15:48:18 +0530

    Update UsersController_v2.php

--- app/Http/Controllers/Api/UsersController_v2.php
+++ app/Http/Controllers/Api/UsersController_v2.php
@@ -910,8 +910,13 @@ class UsersController_v2 extends BaseController
                             if ($approch_check->status == 'accepted') {
                                 $data['is_approach'] = "message";
                             } else if ($approch_check->status == 'pending') {
+                                if ($approch_check->sender_id == $this->user->id) {
 
-                                $data['is_approach'] = "cancel";
+                                    $data['is_approach'] = "cancel";
+                                } else {
+                                    $data['is_approach'] = "accept_reject";
+                                }
+
 
                                 if ($approch_check->type == 'approach') {
 
