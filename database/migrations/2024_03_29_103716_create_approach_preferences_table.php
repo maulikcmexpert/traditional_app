315a78b4cb2e7c44b07880e9bb4f85d12f0c41ca
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-03-29 16:28:47 +0530

    Update 2024_03_29_103716_create_approach_preferences_table.php

--- database/migrations/2024_03_29_103716_create_approach_preferences_table.php
+++ database/migrations/2024_03_29_103716_create_approach_preferences_table.php
@@ -17,11 +17,11 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer("min_age");
             $table->integer("max_age");
-            $table->varchar("religious_preference");
-            $table->varchar("min_weight");
-            $table->varchar("max_weight");
-            $table->varchar("min_height");
-            $table->varchar("max_height");
+            $table->string("religious_preference");
+            $table->string("min_weight");
+            $table->string("max_weight");
+            $table->string("min_height");
+            $table->string("max_height");
             $table->enum("preference_apply_in_search", ['0', '1'])->default('0');
             $table->timestamps();
         });
