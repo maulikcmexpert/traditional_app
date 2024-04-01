fb756192ef5644268c73ec625d70480883ec7618
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-01 11:45:26 +0530

    Update 2024_04_01_060555_update_data_type_preference_apply_in_search_in_approach_preferences.php

--- database/migrations/2024_04_01_060555_update_data_type_preference_apply_in_search_in_approach_preferences.php
+++ database/migrations/2024_04_01_060555_update_data_type_preference_apply_in_search_in_approach_preferences.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('approach_preferences', function (Blueprint $table) {
-            $table->boolean('preference_apply_in_search')->change();
+            $table->boolean('my_boolean_column')->default(false)->change();
         });
     }
 
