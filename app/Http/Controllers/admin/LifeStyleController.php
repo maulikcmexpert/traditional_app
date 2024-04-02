b9986d6d497a8de9541fef97b04a5dc5bd9195b7
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-02 12:11:55 +0530

    commit

---
+++ app/Http/Controllers/admin/LifeStyleController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\DataTables\LifeStyleDataTable;
+use App\Http\Requests\{
+    PostInterestAndHobbies
+};
+use App\Models\{
+    Lifestyle
+};
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class LifeStyleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(LifeStyleDataTable $dataTable)
+    {
+        $page = 'admin.lifestyle.list';
+        $title = 'Interest and hobby';
+        $js = 'admin.lifestyle.scriptjs';
+        return $dataTable->render('layouts.layout', compact('page', 'title', 'js'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -4,14 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\{
     DashboardController,
     UserController,
-    InterestAndHobbiesController
+    InterestAndHobbiesController,
+    LifeStyleController
 };
+
 
 Route::middleware(['admin', 'web', 'auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resources([
         'user' => UserController::class,
         'interest_and_hobby' => InterestAndHobbiesController::class,
+        'lifestyle' => LifeStyleController::class,
     ]);
     Route::post('interest_and_hobby/interest_and_hobby_exist', [InterestAndHobbiesController::class, 'interestAndHobbyExist'])->name('interest_and_hobby.exist');
 });
