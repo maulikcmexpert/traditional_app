d67767836eb9b4e7a0ad0e18b646281cd01bb6b7
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-02 11:24:12 +0530

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Carbon;
+use Carbon\Carbon;
+
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
