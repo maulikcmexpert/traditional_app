7cfe963fa8e40073e487bb3c8f46f91c9a3d4980
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-03 15:41:08 +0530

    commit

--- resources/views/components/admin/footerscript.blade.php
+++ resources/views/components/admin/footerscript.blade.php
@@ -91,8 +91,8 @@
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @stack('scripts')
 
 
