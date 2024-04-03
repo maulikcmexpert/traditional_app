a77c40a1741f8a02d768e32a1b3bf0a59bc5f6eb
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-03 20:10:23 +0530

    commit

--- resources/views/admin/lifestyle/add.blade.php
+++ resources/views/admin/lifestyle/add.blade.php
@@ -25,24 +25,26 @@
                     <div class="live-preview">
                         <form method="POST" id="lifestyle" action="{{ route('lifestyle.store')}}">
                             @csrf
-                            <div class="form-group" id="life_style">
-                                <div class="row gy-4">
-                                    <div class="col-xxl-6 col-md-6">
-                                        <label class="form-label">Lifestyle</label>
-                                        <input type="text" class="form-control lifestyle" name="lifestyle[]" />
 
-                                        <span class="text-danger"> @if ($errors->has('lifestyle*.')){{ $errors->first('lifestyle*.') }} @endif</span>
-
-
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <span class="btn btn-primary" id="addMore"><i class="fa-solid fa-plus"></i></span>
+                            <div class="form-group AddInterest">
+                                <div class="row" id="interest">
+                                    <div class="col-xxl-12 col-md-6">
+                                        <div class="w-100 d-flex align-items-end gap-2">
+                                            <div class="w-100 mb-2 position-relative">
+                                                <label class="form-label">Lifestyle</label>
+                                                <input type="text" class="form-control lifestyle" name="lifestyle[]" />
+                                                <span class="text-danger"> @if ($errors->has('lifestyle*.')){{ $errors->first('lifestyle*.') }} @endif</span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-
+                                <div class="mt-2 mb-2">
+                                    <span class="btn btn-primary" id="addMore"><i class="fa-solid fa-plus"></i></span>
+                                </div>
                             </div>
 
-                            <div><input type="button" id="add" class="btn btn-primary" value="Add"></div>
+                            <div class="text-center"><input type="button" id="add" class="btn btn-primary interestButton" value="Add Lifestyle"></div>
+
 
                         </form>
                         <!--end row-->
@@ -55,20 +57,17 @@
     </div>
 
 </div>
+
 <div id="addMoreData" style="display: none;">
-    <div class="row gy-4">
+    <div class="col-xl-6">
+        <div class="d-flex align-items-end gap-2 mt-2 mb-2">
+            <div class="w-100 position-relative">
+                <label class="form-label">Lifestyle</label>
+                <input type="text" class="form-control lifestyle" name="lifestyle[]" />
 
-        <div class="col-xxl-6 col-md-6">
-            <label class="form-label">Lifestyle</label>
-            <input type="text" class="form-control lifestyle" name="lifestyle[]" />
-
-            <span class="text-danger"> @if($errors->has('lifestyle*.')){{ $errors->first('lifestyle*.') }} @endif</span>
-
-        </div>
-
-        <div class="col-lg-4">
+                <span class="text-danger"> @if($errors->has('lifestyle*.')){{ $errors->first('lifestyle*.') }} @endif</span>
+            </div>
             <span class="btn remove"><i class="fa-solid fa-delete-left"></i></span>
         </div>
-
     </div>
 </div>
