ad43fff82ffed41b27be5edb0ff6a0c0c29ca1b0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-03 19:10:16 +0530

    commit

---
+++ app/DataTables/ZodiacSignDataTable.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\ZodiacSign;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class ZodiacSignDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function ($row) {
+
+                $id = encrypt($row->id);
+                $editRoute = route('zodiacsign.edit', $id);
+                $deleteRoute = route('zodiacsign.destroy', $id);
+
+
+                return '<div><a href="' . $editRoute . '"><i class="fas fa-edit"></i></a>
+            <a href="javascript:" data-url="' . $deleteRoute . '" id="delete"><i class="fas fa-trash text-danger"></i></a></div>
+            ';
+            })
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(ZodiacSign $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+            ->setTableId('zodiacsign-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            //->dom('Bfrtip')
+            ->orderBy(1)
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('excel'),
+                Button::make('csv'),
+                Button::make('pdf'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+
+            Column::make('zodiac_sign'),
+
+            Column::make('action'),
+
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'ZodiacSign_' . date('YmdHis');
+    }
+}
