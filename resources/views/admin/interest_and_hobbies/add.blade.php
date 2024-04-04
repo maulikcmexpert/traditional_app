c7624c34aab1d51f7a0d3218f4e37cf85a328d55
bhavisha-barot <132443595+bhavisha-barot@users.noreply.github.com>
2024-04-04 10:30:46 +0530

    redesign in add more

--- resources/views/admin/interest_and_hobbies/add.blade.php
+++ resources/views/admin/interest_and_hobbies/add.blade.php
@@ -21,11 +21,11 @@
                     <div class="live-preview">
                         <form method="POST" id="interest_and_hobby" action="{{ route('interest_and_hobby.store')}}" enctype="multipart/form-data">
                             @csrf
-                            <div class="form-group AddInterest">
+                            <div class="form-group AddInterest AddMoreForm">
                                 <div class="row" id="interest">
-                                    <div class="col-xxl-12 col-md-6">
+                                    <div class="col-xxl-6 col-md-6 mb-2">
                                         <div class="w-100 d-flex align-items-end gap-2">
-                                            <div class="w-100 mb-2 position-relative">
+                                            <div class="w-100 position-relative">
                                                 <label class="form-label">Interest and hobbies</label>
                                                 <input type="text" class="form-control interest_and_hobby" name="interest_and_hobby[]" />
                                                 <span class="text-danger"> @if ($errors->has('interest_and_hobby*.')){{ $errors->first('interest_and_hobby*.') }} @endif</span>
@@ -33,12 +33,12 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="mt-2 mb-2">
+                                <div class="mt-3 mb-2">
                                     <span class="btn btn-primary" id="addMore"><i class="fa-solid fa-plus"></i></span>
                                 </div>
                             </div>
 
-                            <div class="text-center"><input type="button" id="add" class="btn btn-primary interestButton" value="Add Interest"></div>
+                            <div class="text-center"><input type="button" id="add" class="btn btn-primary submitButton" value="Add Interest"></div>
 
                         </form>
                         <!--end row-->
@@ -52,8 +52,8 @@
 
 </div>
 <div id="addMoreData" style="display: none;">
-    <div class="col-xl-6">
-        <div class="d-flex align-items-end gap-2 mt-2 mb-2">
+    <div class="col-xl-6 mb-2">
+        <div class="d-flex align-items-end gap-2">
             <div class="w-100 position-relative">
                 <label class="form-label">Interest and hobbies</label>
                 <input type="text" class="form-control interest_and_hobby" name="interest_and_hobby[]" />
