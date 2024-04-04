ecc1b76c20a15ba9e9eb47542a26950c9d46b1e5
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-04 18:31:23 +0530

    Update add.blade.php

--- resources/views/admin/verification_object/add.blade.php
+++ resources/views/admin/verification_object/add.blade.php
@@ -45,7 +45,7 @@
 
                         </div>
 
-                        <div class="text-center"><input type="submit" id="add" class="btn btn-primary submitButton" value="Add"></div>
+                        <div class="text-center"><input type="submit" class="btn btn-primary submitButton" value="Add"></div>
 
                     </form>
                     <!--end row-->
