b0903a8605a43ba96d86c12c9ecd1ba6e633c3d1
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-04 17:23:37 +0530

    Update add.blade.php

--- resources/views/admin/verification_object/add.blade.php
+++ resources/views/admin/verification_object/add.blade.php
@@ -32,7 +32,28 @@
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
+                            <div>
+                                <input id="file-upload" type="file" name="fileUpload" accept="image/*" />
+
+                                <label for="file-upload" id="file-drag">
+                                    <img id="file-image" src="#" alt="Preview" class="hidden">
+                                    <div id="start">
+                                        <i class="fa fa-download" aria-hidden="true"></i>
+                                        <div>Select a file or drag here</div>
+                                        <div id="notimage" class="hidden">Please select an image</div>
+                                        <span id="file-upload-btn" class="btn btn-primary">Select a file</span>
+                                    </div>
+                                    <div id="response" class="hidden">
+                                        <div id="messages"></div>
+                                        <progress class="progress" id="file-progress" value="0">
+                                            <span>0</span>%
+                                        </progress>
+                                    </div>
+                                </label>
+                            </div>
+
 
                         </div>
 
