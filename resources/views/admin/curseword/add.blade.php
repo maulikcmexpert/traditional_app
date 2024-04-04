d7fcb34ffe4b8007ad74412062e672a206d041b0
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-04 14:59:47 +0530

    Update add.blade.php

--- resources/views/admin/curseword/add.blade.php
+++ resources/views/admin/curseword/add.blade.php
@@ -38,7 +38,7 @@
                             </div>
                         </div>
 
-                        <div class="text-center"><input type="button" id="add" class="btn btn-primary submitButton" value="Add Interest"></div>
+                        <div class="text-center"><input type="button" id="add" class="btn btn-primary submitButton" value="Add Curse Word"></div>
 
                     </form>
                     <!--end row-->
