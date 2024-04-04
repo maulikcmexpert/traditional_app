14298957d5f1b914ba3710ab4f9ed791f07e17e3
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-04 18:11:29 +0530

    Update scriptjs.blade.php

--- resources/views/admin/verification_object/scriptjs.blade.php
+++ resources/views/admin/verification_object/scriptjs.blade.php
@@ -4,82 +4,9 @@
 
 
 
-        $(document).on('click', '#addMore', function() {
-
-            $("#parentAddBtn").hide();
-            var addMoreData = $("#addMoreData").html();
-            $("#interest").append(addMoreData);
-        });
-        $(document).on('click', '.remove', function() {
-            $(this).parent().parent().remove();
-        });
-        $("#interest .interest_and_hobby").each(function() {
-            $(this).focus(function() {
-                $(this).next("span").text("");
-            });
-        });
-
-
-        $('#add').click(function(e) {
-            e.preventDefault();
-            var promises = [];
-            $('#interest .interest_and_hobby').each(function() {
-                var that = $(this);
-                var thatVal = that.val().trim();
-
-                if (thatVal == '') {
-                    that.next('.text-danger').text('Please enter interest and hobby');
-                } else {
-                    var promise = new Promise(function(resolve, reject) {
-                        $.ajax({
-                            headers: {
-                                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-                            },
-                            dataType: 'Json',
-                            type: "POST",
-                            url: "{{route('interest_and_hobby.exist')}}",
-                            data: {
-                                interest_and_hobby: thatVal
-                            },
-                            success: function(output) {
-                                if (output == false) {
-                                    that.next('.text-danger').text('interest and hobby already exist');
-                                    resolve(false);
-                                } else {
-                                    that.next('.text-danger').text('');
-                                    resolve(true);
-                                }
-                            },
-                            error: function() {
-                                reject("Error occurred");
-                            }
-                        });
-                    });
-                    promises.push(promise);
-                }
-            });
-
-            Promise.all(promises).then(function(results) {
-                if (results.includes(false)) {
-                    // If any result is false, do not submit the form
-                    console.log("Duplicate interest and hobby found");
-                } else if (results.includes(true)) {
-                    // If all results are true, submit the form
-                    console.log("No duplicate interest and hobby, submitting form");
-                    $("#interest_and_hobby").submit();
-                }
-            }).catch(function(error) {
-                console.error("Error occurred during AJAX request:", error);
-            });
-        });
-
-
-
-
-
-        $("#interest_and_hobby").validate({
+        $("#verificationobject").validate({
             rules: {
-                interest_and_hobby: {
+                object_type: {
                     required: true,
                     remote: {
                         headers: {
@@ -87,11 +14,11 @@
                                 "content"
                             ),
                         },
-                        url: "{{route('interest_and_hobby.exist')}}",
+                        url: "{{route('verificationobject.exist')}}",
                         method: "POST",
                         data: {
-                            interest_and_hobby: function() {
-                                return $("input[name='interest_and_hobby']").val();
+                            object_type: function() {
+                                return $("input[name='object_type']").val();
                             },
                             id: function() {
                                 return $("input[name='id']").val();
@@ -99,19 +26,30 @@
                         },
                     },
                 },
-
-            },
-            messages: {
-                interest_and_hobby: {
-                    required: "Please enter interest and hobby",
-                    remote: "Interest and hobby already exist",
+                object_image: {
+                    required: true,
+                    extension: "jpg|jpeg|png|gif", // allowed image extensions
+                    filesize: 1048576, // max file size in bytes (1MB)
                 },
 
             },
-
-
+            messages: {
+                object_type: {
+                    required: "Please enter Object Type",
+                    remote: "Object Type already exist",
+                },
+                object_image: {
+                    required: "Please upload Object Image",
+                    extension: "Please upload a valid image file (jpg, jpeg, png, or gif)",
+                    filesize: "Maximum file size allowed is 1MB",
+                },
+            },
         });
-
+        $.validator.addMethod("filesize", function(value, element, param) {
+            // Convert file size to bytes
+            var size = element.files[0].size;
+            return size <= param;
+        });
 
 
         $(document).on('click', '#edit', function() {
