5a71d56e7b3b54481e14cb76827976affffc7a94
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-04 15:14:09 +0530

    Update CurseWordController.php

--- app/Http/Controllers/admin/CurseWordController.php
+++ app/Http/Controllers/admin/CurseWordController.php
@@ -96,4 +96,45 @@ class CurseWordController extends Controller
     {
         //
     }
+
+    public function CursewordExist(Request $request)
+    {
+        try {
+
+            $eventType = BadWord::where(['words' => $request->words])->get();
+
+            if (count($eventType) > 0) {
+
+                if (isset($request->id) && !empty($request->id)) {
+
+
+
+                    if ($eventType[0]->id == decrypt($request->id)) {
+
+
+
+                        $return =  true;
+
+                        echo json_encode($return);
+
+                        exit;
+                    }
+                }
+
+                $return =  false;
+            } else {
+
+                $return = true;
+            }
+
+            echo json_encode($return);
+
+            exit;
+        } catch (QueryException $e) {
+
+            DB::rollBack();
+
+            return response()->json(false);
+        }
+    }
 }
