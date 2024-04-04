81781c6d658709a603c9e70991dc12f4b4bbaa1c
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-04 17:20:47 +0530

    Update add.blade.php

--- resources/views/admin/verification_object/add.blade.php
+++ resources/views/admin/verification_object/add.blade.php
@@ -5,62 +5,47 @@
         <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
-                <li class="breadcrumb-item"><a href="{{route('interest_and_hobby.index')}}">Interest and hobbies</a></li>
+                <li class="breadcrumb-item"><a href="{{route('verificationobject.index')}}">Interest and hobbies</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Add</li>
             </ol>
         </nav>
     </div>
     <!-- -----header-breadcrumb-end-- -->
-        <div class="container">
-            <div class="card">
-                <div class="card-header align-items-center d-flex">
-                    <h4 class="card-title mb-0 flex-grow-1">Add</h4>
-                </div>
-                <!-- end card header -->
-                <div class="card-body">
-                    <div class="live-preview">
-                        <form method="POST" id="interest_and_hobby" action="{{ route('interest_and_hobby.store')}}" enctype="multipart/form-data">
-                            @csrf
-                            <div class="form-group AddMoreForm">
-                                <div class="row" id="interest">
-                                    <div class="col-xxl-6 col-md-6 mb-2">
-                                        <div class="w-100 d-flex align-items-end gap-2">
-                                            <div class="w-100 position-relative">
-                                                <label class="form-label">Interest and hobbies</label>
-                                                <input type="text" class="form-control interest_and_hobby" name="interest_and_hobby[]" />
-                                                <span class="text-danger"> @if ($errors->has('interest_and_hobby*.')){{ $errors->first('interest_and_hobby*.') }} @endif</span>
-                                            </div>
+    <div class="container">
+        <div class="card">
+            <div class="card-header align-items-center d-flex">
+                <h4 class="card-title mb-0 flex-grow-1">Add</h4>
+            </div>
+            <!-- end card header -->
+            <div class="card-body">
+                <div class="live-preview">
+                    <form method="POST" id="verificationobject" action="{{ route('verificationobject.store')}}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group AddMoreForm">
+                            <div class="row" id="interest">
+                                <div class="col-xxl-6 col-md-6 mb-2">
+                                    <div class="w-100 d-flex align-items-end gap-2">
+                                        <div class="w-100 position-relative">
+                                            <label class="form-label">Object Type</label>
+                                            <input type="text" class="form-control object_type" name="object_type" />
+                                            <span class="text-danger"> @if ($errors->has('object_type*.')){{ $errors->first('object_type*.') }} @endif</span>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="mt-3 mb-2">
-                                    <span class="btn btn-primary" id="addMore"><i class="fa-solid fa-plus"></i></span>
-                                </div>
                             </div>
 
-                            <div class="text-center"><input type="button" id="add" class="btn btn-primary submitButton" value="Add Interest"></div>
+                        </div>
 
-                        </form>
-                        <!--end row-->
-                    </div>
+                        <div class="text-center"><input type="button" id="add" class="btn btn-primary submitButton" value="Add Interest"></div>
 
+                    </form>
+                    <!--end row-->
                 </div>
-            </div>
-        </div>
-        <!--end col-->
-    
 
-</div>
-<div id="addMoreData" style="display: none;">
-    <div class="col-xl-6 mb-2">
-        <div class="d-flex align-items-end gap-2">
-            <div class="w-100 position-relative">
-                <label class="form-label">Interest and hobbies</label>
-                <input type="text" class="form-control interest_and_hobby" name="interest_and_hobby[]" />
-
-                <span class="text-danger"> @if($errors->has('interest_and_hobby*.')){{ $errors->first('interest_and_hobby*.') }} @endif</span>
             </div>
-            <span class="btn remove"><i class="fa-solid fa-delete-left"></i></span>
         </div>
     </div>
+    <!--end col-->
+
+
 </div>
