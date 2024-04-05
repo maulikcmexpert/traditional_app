dcb78a36cf531030894fb0526de514ac4f3bebc5
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-05 18:02:51 +0530

    Update SizeOfOrganizationController.php

--- app/Http/Controllers/admin/SizeOfOrganizationController.php
+++ app/Http/Controllers/admin/SizeOfOrganizationController.php
@@ -4,15 +4,27 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\DataTables\SizeOfOrganizationDataTable;
+use App\Http\Requests\{
+    PostSizeOfOrganization
+};
+use App\Models\{
+    SizeOfOrganization
+};
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 
 class SizeOfOrganizationController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(SizeOfOrganizationDataTable $dataTable)
     {
-        //
+        $page = 'admin.size_of_organization.list';
+        $title = 'Size Of Organization';
+        $js = 'admin.size_of_organization.scriptjs';
+        return $dataTable->render('layouts.layout', compact('page', 'title', 'js'));
     }
 
     /**
