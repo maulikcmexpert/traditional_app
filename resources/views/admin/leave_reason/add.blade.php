e9e465908b6d0c828ab8b28af98007060a484836
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-05 13:27:33 +0530

    comit

---
+++ resources/views/admin/leave_reason/add.blade.php
@@ -0,0 +1,66 @@
+<!-- -----header-breadcrumb-start-- -->
+<div class="page-content form-main-wrp">
+    <div class="header-breadcrumb form-main-wrp">
+
+        <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{route('leavereason.index')}}">Block Reason</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Add</li>
+            </ol>
+        </nav>
+    </div>
+    <!-- -----header-breadcrumb-end-- -->
+    <div class="container">
+        <div class="card">
+            <div class="card-header align-items-center d-flex">
+                <h4 class="card-title mb-0 flex-grow-1">Add</h4>
+            </div>
+            <!-- end card header -->
+            <div class="card-body">
+                <div class="live-preview">
+                    <form method="POST" id="leavereason" action="{{ route('leavereason.store')}}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group AddMoreForm">
+                            <div class="row" id="reason">
+                                <div class="col-xxl-6 col-md-6 mb-2">
+                                    <div class="w-100 d-flex align-items-end gap-2">
+                                        <div class="w-100 position-relative">
+                                            <label class="form-label">Block Reason</label>
+                                            <input type="text" class="form-control reason" name="reason[]" />
+                                            <span class="text-danger"> @if ($errors->has('reason*.')){{ $errors->first('reason*.') }} @endif</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-3 mb-2">
+                                <span class="btn btn-primary" id="addMore"><i class="fa-solid fa-plus"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="text-center"><input type="button" id="add" class="btn btn-primary submitButton" value="Add Block Reason"></div>
+
+                    </form>
+                    <!--end row-->
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!--end col-->
+
+
+</div>
+<div id="addMoreData" style="display: none;">
+    <div class="col-xl-6 mb-2">
+        <div class="d-flex align-items-end gap-2">
+            <div class="w-100 position-relative">
+                <label class="form-label">Leave Reason</label>
+                <input type="text" class="form-control reason" name="reason[]" />
+
+                <span class="text-danger"> @if($errors->has('reason*.')){{ $errors->first('reason*.') }} @endif</span>
+            </div>
+            <span class="btn remove"><i class="fa-solid fa-delete-left"></i></span>
+        </div>
+    </div>
+</div>
---
+++ resources/views/admin/leave_reason/edit.blade.php
@@ -0,0 +1,53 @@
+<!-- -----header-breadcrumb-start-- -->
+<div class="page-content form-main-wrp">
+    <div class="header-breadcrumb form-main-wrp">
+
+        <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{route('leavereason.index')}}">Leave Reason</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Edit</li>
+            </ol>
+        </nav>
+    </div>
+    <!-- -----header-breadcrumb-end-- -->
+
+
+
+    <div class="container">
+        <div class="card">
+            <div class="card-header align-items-center d-flex">
+                <h4 class="card-title mb-0 flex-grow-1">Edit</h4>
+
+            </div>
+            <!-- end card header -->
+            <div class="card-body">
+                <div class="live-preview">
+                    <form method="POST" id="leavereason" action="{{ route('leavereason.update',encrypt($getData->id))}}">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <div class="row gy-4 align-items-end">
+                                <div class="col-xxl-6 col-md-6">
+                                    <label class="form-label">Leave Reason</label>
+                                    <input type="hidden" value="{{encrypt($getData->id)}}" class="form-control leavereason_id" name="id" />
+                                    <input type="text" value="{{$getData->reason}}" class="form-control reason" name="reason" />
+                                    <span class="text-danger"></span>
+                                </div>
+                                <div class="col-xxl-2">
+                                    <input type="button" id="edit" class="btn btn-primary" value="Update">
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </form>
+                    <!--end row-->
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
