0f19d8669b276f42b84e38eb969e17f4909beffa
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-05 17:26:12 +0530

    Update list.blade.php

--- resources/views/admin/religion/list.blade.php
+++ resources/views/admin/religion/list.blade.php
@@ -5,7 +5,7 @@
         <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Block Reason</li>
+                <li class="breadcrumb-item active" aria-current="page">Religion</li>
             </ol>
         </nav>
     </div>
@@ -15,8 +15,8 @@
     <div class="container">
         <div class="card">
             <div class="card-header">
-                <h4 class="card-title">Block Reason</h4>
-                <a href="{{route('blockreason.create')}}" class="btn btn-primary text-right">Add</a>
+                <h4 class="card-title">Religion</h4>
+                <a href="{{route('religion.create')}}" class="btn btn-primary text-right">Add</a>
             </div>
             <div class="card-body">
                 {{ $dataTable->table() }}
