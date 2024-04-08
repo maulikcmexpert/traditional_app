19cf363f2c25ca7bec0726fef5d8cd0c1388365d
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-08 18:59:04 +0530

    Update change_password.blade.php

--- resources/views/admin/change_password.blade.php
+++ resources/views/admin/change_password.blade.php
@@ -1,42 +1,70 @@
-<section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
-        </h2>
+<div class="page-content form-main-wrp">
+    <div class="header-breadcrumb form-main-wrp">
 
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
-        </p>
-    </header>
+        <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{route('generalsetting.index')}}">General Setting</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{($setting == null)? 'Add' : 'Update' }}</li>
+            </ol>
+        </nav>
+    </div>
+    <!-- -----header-breadcrumb-end-- -->
+    <div class="container">
+        <div class="card">
+            <div class="card-header align-items-center d-flex">
+                <h4 class="card-title mb-0 flex-grow-1">{{($setting == null)? 'Add' : 'Update' }}</h4>
+            </div>
+            <!-- end card header -->
+            <div class="card-body">
+                <div class="live-preview">
+                    <form method="POST" id="generalsetting" action="{{ route('password.update') }}">
+                        @csrf
+                        <div class=" form-group AddMoreForm">
+                            <div class="row" id="general_setting">
+                                <div class="col-xxl-4 col-md-6 mb-2">
+                                    <div class="w-100 d-flex align-items-end gap-2">
+                                        <div class="w-100 position-relative">
+                                            <label class="form-label">Current Password</label>
+                                            <input type="text" class="form-control current_password" id="current_password" name="current_password" />
+                                            <span class="text-danger"> @if ($errors->has('current_password')){{ $errors->first('current_password') }} @endif</span>
+                                        </div>
+                                    </div>
+                                </div>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('put')
+                                <div class="col-xxl-4 col-md-6 mb-2">
+                                    <div class="w-100 d-flex align-items-end gap-2">
+                                        <div class="w-100 position-relative">
+                                            <label class="form-label">New Password</label>
+                                            <input type="text" class="form-control new_password" id="new_password" name="new_password" />
+                                            <span class="text-danger"> @if ($errors->has('new_password')){{ $errors->first('new_password') }} @endif</span>
+                                        </div>
+                                    </div>
+                                </div>
 
-        <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+                                <div class="col-xxl-4 col-md-6 mb-2">
+                                    <div class="w-100 d-flex align-items-end gap-2">
+                                        <div class="w-100 position-relative">
+                                            <label class="form-label">Confirm Password</label>
+                                            <input type="text" class="form-control confirm_password" name="confirm_password" />
+                                            <span class="text-danger"> @if ($errors->has('confirm_password')){{ $errors->first('confirm_password') }} @endif</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div class="text-center"><input type="submit" id="add" class="btn btn-primary submitButton" value="Change Password"></div>
+
+                    </form>
+                    <!--end row-->
+                </div>
+
+            </div>
         </div>
+    </div>
+    <!--end col-->
 
-        <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-        </div>
 
-        <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'password-updated')
-            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Saved.') }}</p>
-            @endif
-        </div>
-    </form>
-</section>
+</div>
