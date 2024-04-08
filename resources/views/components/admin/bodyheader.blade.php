2840878ff64d59724acc19dfe7b61e8f519722c4
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-08 18:10:14 +0530

    Update bodyheader.blade.php

--- resources/views/components/admin/bodyheader.blade.php
+++ resources/views/components/admin/bodyheader.blade.php
@@ -583,7 +583,7 @@
                      </div>
                  </a>
                  <ul class="dropdown-menu dropdown-menu-end">
-                     <li><a class="dropdown-item d-flex align-items-center" href="{{ route('password.request')}}"><i class="fa-regular fa-user"></i><span>Change Password</span></a>
+                     <li><a class="dropdown-item d-flex align-items-center" href="{{ route('profile.edit')}}"><i class="fa-regular fa-user"></i><span>Profile</span></a>
                      </li>
 
                      <li>
