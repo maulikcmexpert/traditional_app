483c0b558c2bd6625a01d33c1d2fd9e89a578566
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-08 19:34:41 +0530

    Update PasswordController.php

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\PostChangePassword;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 
 class PasswordController extends Controller
 {
@@ -23,5 +25,30 @@ class PasswordController extends Controller
         ]);
 
         return back()->with('status', 'password-updated');
+    }
+
+    public function checkCurrentPass(Request $request)
+    {
+        try {
+
+            $currentPassword = $request->input('current_password');
+
+            // Get the authenticated user
+            $user = Auth::user();
+
+            // Check if the current password matches the one stored in the database
+            if (Hash::check($currentPassword, $user->password)) {
+                return response()->json(true); // Password matches
+            } else {
+                return response()->json(false); // Password does not match
+            }
+
+            exit;
+        } catch (QueryException $e) {
+
+            DB::rollBack();
+
+            return response()->json(false);
+        }
     }
 }
