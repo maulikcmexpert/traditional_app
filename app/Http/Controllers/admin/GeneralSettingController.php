4c9f7cf0a43c2ce9e54aef8d07607b06fe9a1e9e
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-08 11:37:18 +0530

    Update GeneralSettingController.php

--- app/Http/Controllers/admin/GeneralSettingController.php
+++ app/Http/Controllers/admin/GeneralSettingController.php
@@ -41,7 +41,35 @@ class GeneralSettingController extends Controller
      */
     public function store(PostGeneralSetting $request)
     {
-        dd($request);
+        dd($request->min_age);
+        try {
+
+            DB::beginTransaction();
+
+            if (!empty($request->object_image)) {
+                $image = $request->object_image;
+                $imageName = time() . 'objverify.' . $image->getClientOriginalExtension();
+                $image->move(public_path('storage/verification_object'), $imageName);
+
+                $verifyObj = new VerificationObject();
+                $verifyObj->object_type = $request->object_type;
+                $verifyObj->object_image = $imageName;
+                $verifyObj->save();
+            }
+
+
+            DB::commit();
+            toastr()->success('Verfication Object created successfully !');
+            return redirect()->route('verificationobject.index');
+        } catch (Exception $e) {
+
+            toastr()->error("something went wrong");
+            return redirect()->route('verificationobject.create');
+        } catch (QueryException $e) {
+            DB::rollBack();
+            toastr()->error($e->getMessage());
+            return redirect()->route('verificationobject.create');
+        }
     }
 
     /**
