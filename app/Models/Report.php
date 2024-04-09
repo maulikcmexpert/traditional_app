79c79ee01c12c18f0c5bd673233a6016c05b86cf
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-09 12:45:49 +0530

    commit

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -12,12 +12,12 @@ class Report extends Model
 
     public function reporter_user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'reporter_user_id');
     }
 
 
     public function to_reporter_user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'to_be_reported_user_id');
     }
 }
