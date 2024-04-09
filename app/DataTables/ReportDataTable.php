8de0d7cd554819ec598547dfecaacabd081efdc2
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-09 12:41:45 +0530

    Update ReportDataTable.php

--- app/DataTables/ReportDataTable.php
+++ app/DataTables/ReportDataTable.php
@@ -73,7 +73,7 @@ class ReportDataTable extends DataTable
 
             Column::make('report_user'),
 
-            Column::make('to_be_report_user'),
+            Column::make('to_reporter_user'),
 
         ];
     }
