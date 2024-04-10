01adcced1ab40e8f1bec3106f731212667d17433
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-10 20:27:40 +0530

    Update UserReportController.php

--- app/Http/Controllers/admin/UserReportController.php
+++ app/Http/Controllers/admin/UserReportController.php
@@ -56,7 +56,7 @@ class UserReportController extends Controller
         $page = 'admin.report.chatview';
         $title = 'View Chat Message';
         $userchat =  UserReportChat::with('sender_user')->where('report_id', $report_id)->get()->groupBy('sender_user.full_name');
-
+        dd($userchat);
 
         $firstUser = [];
         $secondUser = [];
