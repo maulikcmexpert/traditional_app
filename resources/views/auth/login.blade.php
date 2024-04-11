41a00c9b7e2289973d8954826649b699fc2e5fee
bhavisha-barot <132443595+bhavisha-barot@users.noreply.github.com>
2024-04-11 19:20:34 +0530

    design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
         <!-- Remember Me -->
         <div class="d-flex justify-content-between  mt-4" style="display:flex; justify-content:space-between">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember" >
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember" style="border:1px solid #6366f1" >
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
             <div>
