c70b87c8aad8bf5bafeb51ce6d62a4e24f5ca50d
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-11 14:51:16 +0530

    commit

--- app/Http/Controllers/Api/UsersController_v2.php
+++ app/Http/Controllers/Api/UsersController_v2.php
@@ -443,7 +443,7 @@ class UsersController_v2 extends BaseController
 
         $lifeStyles = $request->life_styles;
         $interest_and_hobby = $request->interest_and_hobby;
-        $zodiac_sign_id = $request->zodiac_sign_id;
+        $faith_id = $request->faith_id;
 
         if (isset($lifeStyles) && is_array($lifeStyles)) {
             // if exists then delete prev data //
