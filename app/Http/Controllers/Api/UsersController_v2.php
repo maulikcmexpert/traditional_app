2847337323fc8ba64d262b0890442eddab54148c
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-11 15:35:25 +0530

    Update UsersController_v2.php

--- app/Http/Controllers/Api/UsersController_v2.php
+++ app/Http/Controllers/Api/UsersController_v2.php
@@ -649,7 +649,7 @@ class UsersController_v2 extends BaseController
             'email' => $email,
         ];
         $user = User::with(['userdetail', 'user_profile', 'user_lifestyle', 'user_lifestyle.lifestyle', 'user_interest_and_hobby', 'user_interest_and_hobby.interest_and_hobby', 'userdetail.religon', 'userdetail.faith', 'userdetail.state', 'country', 'userdetail.organization', 'user_love_lang'])->where(['id' => $user_id, 'status' => 'active'])->first();
-        dd($user);
+
         if ($user_id) {
             $data['country_code'] = ($user->country->iso != "") ? $user->country->iso : "";
             $data['country_dial_code'] = ($user->country_code != "") ? $user->country_code : "";
