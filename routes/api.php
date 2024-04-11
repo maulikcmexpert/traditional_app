616c91f2a066037b86aaa0118315b64b14b8f742
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-11 16:38:13 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -92,7 +92,7 @@ Route::group(['namespace' => 'Api', 'prefix' => 'v2'], function () {
     Route::get('culture_list', [UsersController_v2::class, 'CultureList']);
     Route::get('Body_type_list', [UsersController_v2::class, 'BodyTypeList']);
     Route::get('daily_activity_list', [UsersController_v2::class, 'DailyActivityList']);
-    Route::get('exersice_list', [UsersController_v2::class, 'ExersiceList']);
+    Route::get('exercise_list', [UsersController_v2::class, 'ExerciseList']);
     Route::get('eating_habit_list', [UsersController_v2::class, 'EatingHabitList']);
     Route::post('otp_verify', [UsersController_v2::class, 'otpVerify'])->name('otp_verify');
 
