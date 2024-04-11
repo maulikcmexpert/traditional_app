30c20881e5ccadf4708ccdc57d9769bddb53ff7e
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-11 18:25:06 +0530

    comit

---
+++ app/Http/Controllers/admin/FaithController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\DataTables\FaithDataTable;
+use App\Http\Requests\{
+    PostFaith
+};
+
+use App\Models\{
+    Faith,
+    UserDetail
+};
+
+use Illuminate\Http\Request;
+
+class FaithController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(FaithDataTable $dataTable)
+    {
+        $page = 'admin.faith.list';
+        $title = 'Faith';
+        $js = 'admin.faith.scriptjs';
+        return $dataTable->render('layouts.layout', compact('page', 'title', 'js'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PostFaith $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+
+    public function FaithExist(Request $request)
+    {
+        try {
+
+            $eventType = Faith::where(['faith' => $request->faith])->get();
+
+            if (count($eventType) > 0) {
+
+                if (isset($request->id) && !empty($request->id)) {
+
+
+
+                    if ($eventType[0]->id == decrypt($request->id)) {
+
+
+
+                        $return =  true;
+
+                        echo json_encode($return);
+
+                        exit;
+                    }
+                }
+
+                $return =  false;
+            } else {
+
+                $return = true;
+            }
+
+            echo json_encode($return);
+
+            exit;
+        } catch (QueryException $e) {
+
+            DB::rollBack();
+
+            return response()->json(false);
+        }
+    }
+
+    public function selectedbyuser(Request $request)
+    {
+        try {
+
+
+            $ids = decrypt($request->id);
+            $eventType = UserDetail::where(['faith_id' => $ids])->get();
+
+            if (count($eventType) > 0) {
+
+                $return =  false;
+            } else {
+
+                $return = true;
+            }
+
+            echo json_encode($return);
+
+            exit;
+        } catch (QueryException $e) {
+
+            DB::rollBack();
+
+            return response()->json(false);
+        }
+    }
+}
---
+++ resources/views/admin/faith/scriptjs.blade.php
@@ -0,0 +1,191 @@
+<script>
+    $(document).ready(function() {
+        // Validate the form with ID 'interest_and_hobbies'
+
+
+
+        $(document).on('click', '#addMore', function() {
+            var addMoreData = $("#addMoreData").html();
+            $("#faiths").append(addMoreData);
+        });
+        $(document).on('click', '.remove', function() {
+            $(this).parent().parent().remove();
+        });
+        $("#faiths .faith").each(function() {
+            $(this).focus(function() {
+                $(this).next("span").text("");
+            });
+        });
+
+
+        $('#add').click(function(e) {
+            e.preventDefault();
+            var promises = [];
+            $('#faiths .faith').each(function() {
+                var that = $(this);
+                var thatVal = that.val().trim();
+
+                if (thatVal == '') {
+                    that.next('.text-danger').text('Please enter Faith');
+                } else {
+                    var promise = new Promise(function(resolve, reject) {
+                        $.ajax({
+                            headers: {
+                                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                            },
+                            dataType: 'Json',
+                            type: "POST",
+                            url: "{{route('faith.exist')}}",
+                            data: {
+                                faith: thatVal
+                            },
+                            success: function(output) {
+                                if (output == false) {
+                                    that.next('.text-danger').text('Faith already exist');
+                                    resolve(false);
+                                } else {
+                                    that.next('.text-danger').text('');
+                                    resolve(true);
+                                }
+                            },
+                            error: function() {
+                                reject("Error occurred");
+                            }
+                        });
+                    });
+                    promises.push(promise);
+                }
+            });
+
+            Promise.all(promises).then(function(results) {
+                if (results.includes(false)) {
+                    // If any result is false, do not submit the form
+                    console.log("Duplicate Faith found");
+                } else if (results.includes(true)) {
+                    // If all results are true, submit the form
+                    console.log("No duplicate Faith, submitting form");
+                    $("#faith").submit();
+                }
+            }).catch(function(error) {
+                console.error("Error occurred during AJAX request:", error);
+            });
+        });
+
+
+
+
+
+        $("#faith").validate({
+            rules: {
+                zodiac_sign: {
+                    required: true,
+                    remote: {
+                        headers: {
+                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                                "content"
+                            ),
+                        },
+                        url: "{{route('faith.exist')}}",
+                        method: "POST",
+                        data: {
+                            faith: function() {
+                                return $("input[name='faith']").val();
+                            },
+                            id: function() {
+                                return $("input[name='id']").val();
+                            },
+                        },
+                    },
+                },
+
+            },
+            messages: {
+                faith: {
+                    required: "Please enter Faith",
+                    remote: "Faith already exist",
+                },
+
+            },
+
+
+        });
+
+
+
+        $(document).on('click', '#edit', function() {
+            if ($("#faith").valid()) {
+                $("#faith").submit();
+            }
+        })
+
+
+        $(document).on("click", "#delete", function(event) {
+            var userURL = $(this).data("url");
+            var id = $(this).data("id");
+
+            event.preventDefault();
+
+            $.ajax({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                        "content"
+                    ),
+                },
+                method: "post",
+                url: "{{route('faith.selectedbyuser')}}",
+                data: {
+
+                    id: id
+                },
+                dataType: "json",
+                success: function(output) {
+                    if (output == false) {
+                        errorAlert('Faith');
+
+                    } else {
+                        swal({
+                            title: `Are you sure you want to delete this record?`,
+                            text: "If you delete this, it will be gone forever.",
+                            icon: "warning",
+                            buttons: true,
+                            dangerMode: true,
+                        }).then((willDelete) => {
+                            if (willDelete) {
+                                $.ajax({
+                                    headers: {
+                                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
+                                            "content"
+                                        ),
+                                    },
+                                    method: "DELETE",
+                                    url: userURL,
+                                    dataType: "json",
+                                    success: function(output) {
+                                        if (output == true) {
+
+                                            sessionStorage.setItem('showSuccessNotification', 'true');
+                                            location.reload();
+
+
+                                        } else {
+                                            errorAlert('Faith');
+                                        }
+                                    },
+                                });
+                            }
+                        });
+                    }
+                }
+            });
+
+        });
+        if (sessionStorage.getItem('showSuccessNotification')) {
+            // Show the success notification using Toastr
+
+            toastr.success("Faith deleted successfully !");
+            // Remove the flag from sessionStorage
+            sessionStorage.removeItem('showSuccessNotification');
+        }
+
+    });
+</script>
