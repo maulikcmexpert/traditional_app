abb0c636bb41578e4bfc15411f692f2a1d80910e
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-12 17:47:18 +0530

    Update UsersController_v2.php

--- app/Http/Controllers/Api/UsersController_v2.php
+++ app/Http/Controllers/Api/UsersController_v2.php
@@ -793,6 +793,7 @@ class UsersController_v2 extends BaseController
         if ($isRefresh == false) {
             $Id1 = $this->user->id;
             $Id2 = $user_id;
+            dd($Id1, $Id2);
             $already_friend = ApproachRequest::where(function ($query) use ($Id1, $Id2) {
                 $query->where(function ($query) use ($Id1, $Id2) {
                     $query->where('sender_id', $Id1)
