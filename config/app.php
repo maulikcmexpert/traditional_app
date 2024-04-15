47a1cbcab91459e9f1effe5a97f2f7770d56636d
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-15 13:44:29 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class
 
 
 
@@ -188,6 +188,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Image' => Intervention\Image\Facades\Image::class
 
     ])->toArray(),
 
