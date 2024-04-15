6d12239f843341961f36f561ef0ab9b57e9e7465
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-15 11:34:53 +0530

    Update UsersController_v2.php

--- app/Http/Controllers/Api/UsersController_v2.php
+++ app/Http/Controllers/Api/UsersController_v2.php
@@ -94,7 +94,7 @@ class UsersController_v2 extends BaseController
         $twentyFourHoursAgo = $now->subHours(24)->timestamp;
         $database = Firebase::database();
         $data = $database->getReference('/Overview')->getValue();
-        dd($data);
+        dd($data['timeStamp']);
     }
 
     public function userSignup(UserValidate $request)
