19e93a3f2806794be2708a4aaf66796f482bfe23
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-15 15:47:34 +0530

    Create 2024_04_15_101530_add_no_chat_day_duration_status_field_in_table.php

---
+++ database/migrations/2024_04_15_101530_add_no_chat_day_duration_status_field_in_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->integer("no_chat_day_duration")->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+        });
+    }
+};
