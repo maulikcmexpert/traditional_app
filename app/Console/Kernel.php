9c4f5d15f62117c15fb231a48d3da81d6916f7da
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-17 18:53:30 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('delete:old-messages')->hourly(); // Run the command daily
+        $schedule->command('delete:old-messages')->everyMinute(); // Run the command daily
 
     }
 
