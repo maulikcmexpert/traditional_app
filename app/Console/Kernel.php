afecdf3cee23d1e0e78d14c6272226fc16763dba
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-17 19:28:51 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,16 +4,20 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\DeleteOldMessages;
+use FireStore\Commands;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      */
+
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('delete:old-messages')->everyMinute(); // Run the command daily
+
+        $schedule->command('app:delete-old-messages')->everyMinute(); // Run the command daily
 
     }
 
