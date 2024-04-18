b43a02bbc64741be72725d69315b5fb261a1cc24
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-18 11:02:59 +0530

    Create NameValidation.php

---
+++ app/Rules/NameValidation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class NameValidation implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+
+        if (ctype_digit(trim($value))) {
+            $fail("Please enter valid " . $attribute);
+        }
+
+        if (preg_match("/^[^a-zA-Z0-9 ]+$/", trim($value))) {
+            $fail("Please enter valid Full Name");
+        }
+
+        if (preg_match("/^[0-9@#$%^&*()_+=\[\]{};:,.<>?|\\/-]+$/", $value)) {
+            $fail("Please enter valid Full Name");
+        }
+    }
+}
