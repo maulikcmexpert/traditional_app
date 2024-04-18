6a989da4cbbae6ad887e6655d7f903903c96a1c2
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-18 18:23:00 +0530

    Update add.blade.php

--- resources/views/admin/legal_agreement/add.blade.php
+++ resources/views/admin/legal_agreement/add.blade.php
@@ -13,7 +13,7 @@
     <div class="">
         <div class="card">
             <div class="card-header align-items-center d-flex">
-                <h4 class="card-title mb-0 flex-grow-1">{{($setting == null)? 'Add' : 'Update' }}</h4>
+                <h4 class="card-title mb-0 flex-grow-1">{{($legalAgreement == null)? 'Add' : 'Update' }}</h4>
             </div>
             <!-- end card header -->
             <div class="card-body">
@@ -25,9 +25,9 @@
                                 <div class="col-xxl-2 col-md-3 mb-2">
                                     <div class="w-100 d-flex align-items-end gap-2">
                                         <div class="w-100 position-relative">
-                                            <label class="form-label">Android Version</label>
-                                            <input type="text" class="form-control android_version" id="android_version" name="android_version" value="{{($setting == null)? '':$setting->android_version }}" />
-                                            <span class="text-danger"> @if ($errors->has('android_version')){{ $errors->first('android_version') }} @endif</span>
+                                            <label class="form-label">Privacy Policy</label>
+                                            <input type="text" class="form-control privacy_policy" id="privacy_policy" name="privacy_policy" value="{{($legalAgreement == null)? '':$legalAgreement->privacy_policy }}" />
+                                            <span class="text-danger"> @if ($errors->has('privacy_policy')){{ $errors->first('privacy_policy') }} @endif</span>
                                         </div>
                                     </div>
                                 </div>
@@ -35,42 +35,18 @@
                                 <div class="col-xxl-2 col-md-3 mb-2">
                                     <div class="w-100 d-flex align-items-end gap-2">
                                         <div class="w-100 position-relative">
-                                            <label class="form-label">Android In Force</label>
-                                            <select class="form-control" name="android_in_force" id="android_in_force">
-                                                <option value="0">0</option>
-                                                <option value="1">1</option>
-                                            </select>
-                                            <span class="text-danger"> @if ($errors->has('android_in_force')){{ $errors->first('android_in_force') }} @endif</span>
+                                            <label class="form-label">Term And Condition</label>
+                                            <input type="text" class="form-control term_and_condition" id="term_and_condition" name="term_and_condition" value="{{($legalAgreement == null)? '':$legalAgreement->term_and_condition }}" />
+                                            <span class="text-danger"> @if ($errors->has('term_and_condition')){{ $errors->first('term_and_condition') }} @endif</span>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="col-xxl-2 col-md-3 mb-2">
-                                    <div class="w-100 d-flex align-items-end gap-2">
-                                        <div class="w-100 position-relative">
-                                            <label class="form-label">Ios Version</label>
-                                            <input type="text" class="form-control ios_version" id="ios_version" name="ios_version" value="{{($setting == null)? '':$setting->ios_version }}" />
-                                            <span class="text-danger"> @if ($errors->has('ios_version')){{ $errors->first('ios_version') }} @endif</span>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div class="col-xxl-2 col-md-3 mb-2">
-                                    <div class="w-100 d-flex align-items-end gap-2">
-                                        <div class="w-100 position-relative">
-                                            <label class="form-label">Ios In Force</label>
-                                            <select class="form-control" name="ios_in_force" id="ios_in_force">
-                                                <option value="0">0</option>
-                                                <option value="1">1</option>
-                                            </select>
-                                            <span class="text-danger"> @if ($errors->has('ios_in_force')){{ $errors->first('ios_in_force') }} @endif</span>
-                                        </div>
-                                    </div>
-                                </div>
 
                             </div>
                         </div>
-                        <div class="text-center"><input type="submit" id="add" class="btn btn-primary submitButton" value="{{($setting == null)? 'Add' : 'Update' }}"></div>
+                        <div class="text-center"><input type="submit" id="add" class="btn btn-primary submitButton" value="{{($legalAgreement == null)? 'Add' : 'Update' }}"></div>
 
                     </form>
                     <!--end row-->
