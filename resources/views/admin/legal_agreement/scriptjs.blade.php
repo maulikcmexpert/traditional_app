c4c67e4d4852180293515c2decd0bcc14e0dc1ea
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-18 19:13:10 +0530

    Update scriptjs.blade.php

--- resources/views/admin/legal_agreement/scriptjs.blade.php
+++ resources/views/admin/legal_agreement/scriptjs.blade.php
@@ -5,36 +5,27 @@
 
         // Form Validation
         $("#legal_agreement").validate({
-
             ignore: [],
             debug: false,
             rules: {
+
                 privacy_policy: {
                     required: function() {
                         CKEDITOR.instances.privacy_policy.updateElement();
                     },
-                },
-                term_and_condition: {
-                    required: function() {
-                        CKEDITOR.instances.term_and_condition.updateElement();
-                    },
-                },
-            },
-            messages: {
-                privacy_policy: {
-                    required: "Please enter Privacy Policy",
-                },
-                term_and_condition: {
-                    required: "Please enter Term And Condition",
+
+
                 }
             },
-            errorPlacement: function(error, element) {
-                error.appendTo(element.closest(".position-relative").find(".text-danger"));
+            messages: {
+
+                privacy_policy: {
+                    required: "Please enter Text",
+
+
+
+                }
             }
         });
-
-
-
-
     });
 </script>
