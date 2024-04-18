13c2821682d51831e514e5620e649dd05b0b0e74
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-18 19:24:40 +0530

    Update scriptjs.blade.php

--- resources/views/admin/legal_agreement/scriptjs.blade.php
+++ resources/views/admin/legal_agreement/scriptjs.blade.php
@@ -9,10 +9,7 @@
             rules: {
 
                 privacy_policy: {
-                    required: function() {
-                        CKEDITOR.instances.privacy_policy.updateElement(); // Update the textarea value before validation
-                        return CKEDITOR.instances.editor1.getData().trim() === '';
-                    },
+                    required: true
 
 
                 }
