95b5e14e60b775722357032d3edc8bf8d9c11575
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-18 16:22:13 +0530

    comit

--- resources/views/admin/general_setting/scriptjs.blade.php
+++ resources/views/admin/general_setting/scriptjs.blade.php
@@ -8,9 +8,9 @@
         var minAge = parseInt($("#min_age").val());
         var maxAge = parseInt($("#max_age").val());
         if (minAge >= maxAge) {
-            return "Minimum age must be less than max age";
+            return "Min Age must be less than max age";
         } else {
-            return "Maximum age must be greater than min age";
+            return "Max Age must be greater than min age";
         }
     });
     $(document).ready(function() {
@@ -20,42 +20,50 @@
             rules: {
                 min_age: {
                     required: true,
-                    minMaxAgeComparison: true
+                    minMaxAgeComparison: true,
+                    numeric: true
                 },
                 max_age: {
                     required: true,
-                    minMaxAgeComparison: true
+                    minMaxAgeComparison: true,
+                    numeric: true
                 },
 
                 ghost_count: {
 
                     required: true,
+                    numeric: true
                 },
 
                 ghost_day: {
-
                     required: true,
+                    numeric: true
                 },
             },
             messages: {
                 min_age: {
-                    required: "Please enter min age",
-
+                    required: "Please enter Min Age",
+                    numeric: "Please enter in digit"
                 },
                 max_age: {
-                    required: "Please enter max age",
-
+                    required: "Please enter Max Age",
+                    numeric: "Please enter in digit"
                 },
 
                 ghost_count: {
-
-                    required: "Please enter ghost count",
+                    required: "Please enter Ghost Count",
+                    numeric: "Please enter in digit"
                 },
 
                 ghost_day: {
-
-                    required: "Please enter ghost day",
+                    required: "Please enter Ghost Day",
+                    numeric: "Please enter in digit"
                 },
+                no_chat_day_duration: {
+                    required: "Please enter No Chat Day Duration",
+                    numeric: "Please enter in digit"
+
+                }
             },
         });
 
