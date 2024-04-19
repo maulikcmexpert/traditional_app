81084c931646e4a0bf650c6642cb8eaa18c25cfe
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-19 20:44:10 +0530

    Update add.blade.php

--- resources/views/admin/leave_reason/add.blade.php
+++ resources/views/admin/leave_reason/add.blade.php
@@ -5,7 +5,7 @@
         <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
-                <li class="breadcrumb-item"><a href="{{route('leavereason.index')}}">Block Reason</a></li>
+                <li class="breadcrumb-item"><a href="{{route('leavereason.index')}}">Leave Reason</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Add</li>
             </ol>
         </nav>
