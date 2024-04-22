82eb37ac42dabf58aeac52d301652283ca0e5294
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-22 15:01:09 +0530

    Update NameValidation.php

--- app/Rules/NameValidation.php
+++ app/Rules/NameValidation.php
@@ -14,7 +14,7 @@ class NameValidation implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-
+        dd($attribute);
         if ($attribute == 'interest_and_hobby') {
             $attribute = "Interest and Hobby";
         }
