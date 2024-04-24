f500408933c1a3cd44a759488f1054bc17aee9a5
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-04-24 15:54:43 +0530

    Update ReportDataTable.php

--- app/DataTables/ReportDataTable.php
+++ app/DataTables/ReportDataTable.php
@@ -45,7 +45,7 @@ class ReportDataTable extends DataTable
                 return '<div class="d-flex gap-2"><a class="editBtns" href="' . $view . '"><i class="fas fa-comment"></i></a></div>
             ';
             })
-            ->rawColumns(['report_user', 'to_be_reporter_user', 'report_media', 'action'])
+            ->rawColumns(['report_user:full_name', 'to_be_reporter_user:full_name', 'report_media', 'action'])
             ->setRowId('id');
     }
 
