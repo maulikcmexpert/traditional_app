d004c98c0e6a70895a96dec84ad6c05d31f4ad23
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-05-24 19:36:06 +0530

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -26,7 +26,7 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
 
-        dd($request->authenticate());
+
         $request->authenticate();
 
         $request->session()->regenerate();
