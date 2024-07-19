40ec5debf8346a0ec676cfe89da20c7a588d59d1
NidhiCME <nidhi.cmexpertise@gmail.com>
2024-07-19 12:40:21 +0530

    Update getData.php

--- app/Helper/getData.php
+++ app/Helper/getData.php
@@ -296,16 +296,20 @@ function getSearchUser($filter, $page, $user_id)
 
     // Apply filters based on search criteria
     $query->with('userdetail');
-    $query->where('full_name', 'like', "%$search_name%");
-    $query->where('city', 'like', "%$search_name%");
+    // $query->where('full_name', 'like', "%$search_name%");
     $query->where('user_type', 'user');
     $query->where('status', 'active');
 
     if (!empty($city)) {
 
-        $query->whereHas('userdetail', function ($q) use ($city) {
-            $q->Where('city', 'like', "%$city%");
-        });
+        $query->whereHas('userdetail', function ($q) use ($city, $search_name) {
+            $q->Where('city', 'like', "%$city%")
+                ->orWhere('city', 'like', "%$search_name%");
+        })->orWhere('full_name', 'like', "%$search_name%");
+    } else {
+        $query->whereHas('userdetail', function ($q) use ($search_name) {
+            $q->where('city', 'like', "%$search_name%");
+        })->orWhere('full_name', 'like', "%$search_name%");
     }
 
     if ($organizationName != "") {
